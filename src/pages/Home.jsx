68f2763f453ed15ed06adb462376c5cf0a97a500
Florence Ndemi <ndemiflorence@gmail.com>
2025-07-12 05:45:18 +0300

    Added signatureDishes to Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,19 +4,20 @@ import HeroCarousel from "../components/hero";
 import OurStory from "../components/story";
 import Footer from "../components/footer";
 import Chatbot from "../components/chatbot"; // Import the Chatbot component
+import SignatureDishes from "../components/signature-dishes";
 
 export default function Home() {
   const [isChatbotOpen, setIsChatbotOpen] = useState(false);
-
   const toggleChatbot = () => {
     setIsChatbotOpen(!isChatbotOpen);
   };
-
   return (
     <>
       <Navbar />
       <HeroCarousel />
       <OurStory onOpenChatbot={toggleChatbot} />{" "}
+            <SignatureDishes/>
+
       {/* Pass the toggle function to OurStory */}
       <Footer />
       {/* Render the Chatbot component and pass the state and setter */}
